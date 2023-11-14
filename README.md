6c01a725db95d86b42a9c5cf5f95e7d73bc2bb02
Frederik <frho@its.aau.dk>
2023-11-14 14:00:04 +0100

    getting ready

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Advent-of-code-2023
+
+#Getting started:
+
+`python -m venv advent`
+`source advent/bin/activate`
+`pip install -r requirements.txt`
+
+Click commands:
+
+`python 01.py test`
+`python 01.py solve`
+`python 01.py submit`
---
+++ template.py
@@ -0,0 +1,47 @@
+from collections import Counter, defaultdict
+
+import click
+from aocd import data, lines, numbers
+from aocd import submit as aocd_submit
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.version_option(version="0.0.1")
+def advent():
+    pass
+
+
+def a_solver(input):
+    pass
+
+
+def b_solver(input):
+    pass
+
+
+@advent.command()
+def test():
+    test_input = [
+
+    ]
+
+    print("A result:", a_solver(test_input))
+    print("B result:", b_solver(test_input))
+
+
+@advent.command()
+def solve():
+    print("A result:", a_solver(lines))
+    print("B result:", b_solver(lines))
+
+
+@advent.command()
+def submit():
+    aocd_submit(a_solver(lines), part="a")
+    aocd_submit(b_solver(lines), part="b")
+
+
+if __name__ == "__main__":
+    advent()
